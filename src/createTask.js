658de05003b5e20e3bc677592c5c8e62520f597c
Jessica Nguyen <jessicaawhy@gmail.com>
2020-10-12 23:53:59 -0700

    Create class modules for project and tasks

---
+++ src/createTask.js
@@ -0,0 +1,11 @@
+class Task {
+  constructor(title, description, dueDate, priority, status) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.status = status;
+  }
+}
+
+export { Task };

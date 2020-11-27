0106188eac06c8ad2aa6ac2d64f88fc54523e84c
Jessica Nguyen <jessicaawhy@gmail.com>
2020-11-26 23:37:57 -0800

    Fix bug for when there is no current project

--- src/index.js
+++ src/index.js
@@ -56,41 +56,53 @@ function render(obj) {
     deleteButton.addEventListener('click', (e) => deleteProject(e));
   }
   
-  let header = document.getElementById('main-project-header');
-  header.innerHTML = current;
-  let container = document.getElementById('main-container');
-  container.style.display = 'block';
+  if (current) {
+    let header = document.getElementById('main-project-header');
+    header.innerHTML = current;
+    let container = document.getElementById('main-container');
+    container.style.display = 'block';
+    let taskButton = document.getElementById('add-todo-button');
+    taskButton.style.display = 'block';
+  
+    for (let i = 0; i < projectObj[current].length; i++) {
+      let container = create({
+        type: 'div', 
+        innerHTML: '',
+        attributes: {
+          id: ''
+        },
+        classList: [],
+        parentID: 'todo-list',
+      })
+      
+      let todo = create({
+        type: 'li', 
+        innerHTML: `${projectObj[current][i]}`,
+        attributes: {
+          id: ''
+        },
+        classList: [],
+        parentElement: container,
+      })
+      
+      let deleteButton = create({
+        type: 'span', 
+        innerHTML: 'x',
+        attributes: {
+          id: ''
+        },
+        classList: [],
+        parentElement: container,
+      })
+    }
+  } else {
+    let header = document.getElementById('main-project-header');
+    header.innerHTML = "Add a new project!";
+    let container = document.getElementById('main-container');
+    container.style.display = 'block';
+    let taskButton = document.getElementById('add-todo-button');
+    taskButton.style.display = 'none';
 
-  for (let i = 0; i < projectObj[current].length; i++) {
-    let container = create({
-      type: 'div', 
-      innerHTML: '',
-      attributes: {
-        id: ''
-      },
-      classList: [],
-      parentID: 'todo-list',
-    })
-    
-    let todo = create({
-      type: 'li', 
-      innerHTML: `${projectObj[current][i]}`,
-      attributes: {
-        id: ''
-      },
-      classList: [],
-      parentElement: container,
-    })
-    
-    let deleteButton = create({
-      type: 'span', 
-      innerHTML: 'x',
-      attributes: {
-        id: ''
-      },
-      classList: [],
-      parentElement: container,
-    })
   }
 }
 

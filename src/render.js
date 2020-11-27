234e61a80054b0ddefd645f1ea6f96173b847e1a
Jessica Nguyen <jessicaawhy@gmail.com>
2020-11-27 14:32:33 -0800

    Refactor functions

--- src/render.js
+++ src/render.js
@@ -1,29 +1,8 @@
-import { createMain, createSidebar, create } from './createPage';
-import { addModalListeners } from './modal';
-
-// createSidebar();
-// createMain();
-addModalListeners();
-
-const projectObj = {
-  Coding: [
-    'Read articles',
-    'Do challenge problems',
-    'Daily commit'
-  ],
-  Chores: [
-    'Get groceries',
-    'Do laundry',
-    'Wash dishes'
-  ]
-}
-
-let current = Object.keys(projectObj)[0];
-
 function render(obj) {
   deleteElements();
   renderSidebar();
   renderMain();
+  addButtonListeners();
 }
 
 function deleteElements() {
@@ -74,9 +53,6 @@ function renderSidebar(){
       classList: [],
       parentElement: container,
     })
-
-    project.addEventListener('click', (e) => switchProjectView(e));
-    deleteButton.addEventListener('click', (e) => deleteItem(e));
   }
 }
 
@@ -119,9 +95,8 @@ function renderMain(){
         classList: [],
         parentElement: container,
       })
-
-      deleteButton.addEventListener('click', (e) => deleteItem(e));
     }
+
   } else {
     let header = document.getElementById('main-project-header');
     header.innerHTML = "Add a new project!";
@@ -132,55 +107,6 @@ function renderMain(){
   }
 }
 
-function switchProjectView(e) {
-  current = e.target.innerHTML;
-  
-  render(projectObj);
+function addButtonListeners() {
+
 }
-
-function addNewItem(e) {
-  let target = e.target.id
-  if (target === 'submit-todo') {
-    let todo = document.getElementById('todo-name').value;
-    document.getElementById('todo-name').value = '';
-    projectObj[current].push(todo)
-  } else if (target === 'submit-project') {
-    let project = document.getElementById('project-name').value;
-    document.getElementById('project-name').value = '';
-    projectObj[project] = ['Add tasks here!'];
-    current = project;
-  }
-
-  const modal = document.getElementById('todo-modal');
-  modal.style.display = "none";
-
-  render(projectObj);
-}
-
-function deleteItem(e) {
-  let container = e.target.closest('ul').id;
-
-  if (container === 'todo-list') {
-    let target = e.target.parentElement.firstChild.innerHTML;
-    let index = projectObj[current].indexOf(target);
-
-    projectObj[current].splice(index, 1);
-  } else if (container === 'project-list') {
-    let target = e.target.parentElement.firstChild.innerHTML;
-
-    delete projectObj[target];
-    if (current === target) {
-      current = Object.keys(projectObj)[0];
-    }
-  }
-
-  render(projectObj);
-}
-
-let submitButton = document.getElementById('submit-project');
-submitButton.addEventListener('click', (e) => addNewItem(e));
-let todoButton = document.getElementById('submit-todo');
-todoButton.addEventListener('click', (e) => addNewItem(e));
-
-render(projectObj);
-
